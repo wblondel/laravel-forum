6eb65e9a01305ef0038cbbe2732c30ead7899614
William Gérald Blondel <contact@williamblondel.fr>
2024-05-25 23:38:12 +0800

    Refactor macros

--- tests/Feature/Controllers/ThreadController/IndexTest.php
+++ tests/Feature/Controllers/ThreadController/IndexTest.php
@@ -2,8 +2,6 @@
 
 use App\Http\Resources\ThreadResource;
 use App\Models\Thread;
-use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Http\Resources\Json\ResourceCollection;
 use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\get;
@@ -16,34 +14,6 @@ it('should return the correct component', function () {
 });
 
 it('passes threads to the view', function () {
-    AssertableInertia::macro('hasResource', function (string $key, JsonResource $resource) {
-        $props = $this->toArray()['props'];
-
-        $compiledResource = $resource->response()->getData(true);
-
-        expect($props)
-            ->toHaveKey($key, message: "Key \"{$key}\" not passed as a property to Inertia.")
-            ->and($props[$key])
-            ->toEqual($compiledResource);
-
-        return $this;
-    });
-
-    AssertableInertia::macro('hasPaginatedResource', function (string $key, ResourceCollection $resourceCollection) {
-        $props = $this->toArray()['props'];
-
-        $compiledResource = $resourceCollection->response()->getData(true);
-
-        expect($props)
-            ->toHaveKey($key, message: "Key \"{$key}\" not passed as a property to Inertia.")
-            ->and($props[$key])
-            ->toHaveKeys(['data', 'links', 'meta'])
-            ->data
-            ->toEqual($compiledResource['data']);
-
-        return $this;
-    });
-
     $threads = Thread::factory(5)->hasPosts(10)->create();
 
     $paginatedResource = ThreadResource::collection($threads
@@ -58,7 +28,5 @@ it('passes threads to the view', function () {
     );
 
     get(route('threads.index'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-            ->hasPaginatedResource('threads', $paginatedResource)
-        );
+        ->assertHasPaginatedResource('threads', $paginatedResource);
 });
